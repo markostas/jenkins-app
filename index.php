1f7c9dc5e3dc6ab682c72ec9a37e194dcc1318b8
markostas <popkornello@icloud.com>
2021-10-08 02:01:04 +0300

    test push 2.0

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ else
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>PHP APPLICATION - AWS Elastic Beanstalk</title>
+    <title>PHP with AWS Elastic Beanstalk</title>
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
     <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
@@ -24,7 +24,7 @@ else
 <body>
     <section class="congratulations">
         <h1>FINAL PROJECT</h1>
-        <p>MY AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
+        <p>AWS Elastic Beanstalk successfully deployed <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>We are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
     </section>
